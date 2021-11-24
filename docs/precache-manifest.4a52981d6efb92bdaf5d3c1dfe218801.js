2a1a96c183b1779df9ff989787a6e0f4a2f089f8
Janyel <develloperjam.97@gmail.com>
2021-11-23 19:49:02 -0500

    first commit

---
+++ docs/precache-manifest.4a52981d6efb92bdaf5d3c1dfe218801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f794d4cde971af43aea4dac9f3ffda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "093faaad7b9b2b187bad",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093faaad7b9b2b187bad",
+    "url": "/static/js/main.2f2a6d99.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
